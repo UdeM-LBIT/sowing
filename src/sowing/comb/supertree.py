7c27d7db7a11711d79c71905a830ef47a05cc3ad
Mattéo Delabre <git.matteo@delab.re>
2024-08-12 00:08:54 -0400

    supertree: Make build() yield all compatible supertrees

--- src/sowing/comb/supertree.py
+++ src/sowing/comb/supertree.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
-from collections.abc import Set
+from typing import Set, Generator
+from itertools import product
 from .. import traversal
 from ..node import Node
 from ..zipper import Zipper
@@ -73,42 +74,8 @@ def breakup(root: Node) -> tuple[list[Node], list[Triple], list[Fan]]:
     return leaves, triples, fans
 
 
-def build(
-    leaves: list[Node],
-    triples: list[Triple] = [],
-    fans: list[Fan] = [],
-) -> Node | None:
-    """
-    Construct a phylogenetic tree satisfying the topology constraints given by
-    a set of triples and fans.
-
-    The returned tree is the smallest tree (in number of nodes) compatible with
-    all the triples and fans given as input, if such a tree exists.
-
-    This implements the OneTree algorithm from [Ng and Wormald, 1996].
-
-    :param leaves: set of leaves
-    :param triples: set of triples
-    :param fans: set of fans
-    :returns: constructed tree, or None if the constraints are inconsistent
-    """
-    if not leaves:
-        return None
-
-    if len(leaves) == 1:
-        return leaves[0]
-
-    if len(leaves) == 2:
-        left, right = leaves
-        return Node().add(left).add(right)
-
-    partition = Partition(leaves)
-
-    # Merge groups for triples
-    for triple in triples:
-        partition.union(*triple.ingroup)
-
-    # Ensure fans are all in the same group or all in different groups
+def _build_merge_fans(partition, fans):
+    """Ensure fans are either all in the same group or all in different groups."""
     merged = True
 
     while merged:
@@ -116,39 +83,86 @@ def build(
 
         for fan in fans:
             leaves = list(fan.group)
-            roots = set(map(lambda leaf: partition.find(leaf), leaves))
+            roots = {partition.find(leaf) for leaf in leaves}
 
             if len(roots) < len(leaves):
                 merged = merged or partition.union(*leaves)
 
-    if len(partition) <= 1:
-        return None
 
-    # Recursively build subtrees for each group
-    root = Node()
+def build(
+    leaves: list[Node],
+    triples: list[Triple] = [],
+    fans: list[Fan] = [],
+    arity: int = 0,
+) -> Generator[Node, None, None]:
+    """
+    Enumerate phylogenetic trees satisfying the topology constraints given by
+    a set of triples and fans.
 
-    for group in partition.groups():
-        subtriples = [triple for triple in triples if triple.is_in(group)]
-        subfans = [fan for fan in fans if fan.is_in(group)]
-        subtree = build(group, subtriples, subfans)
+    The first returned tree is the smallest tree (in number of nodes) compatible
+    with all the triples and fans given as input, if such a tree exists.
 
-        if subtree is None:
-            return None
+    This implements the AllTrees algorithm from [Ng and Wormald, 1996].
 
-        root = root.add(subtree)
+    :param leaves: set of leaves
+    :param triples: set of triples
+    :param fans: set of fans
+    :param arity: arity of the generated trees, or 0 to generate trees of arbitrary arity
+    :yields: possible trees, if any
+    """
+    if not leaves:
+        return
 
-    return root
+    if len(leaves) == 1:
+        yield leaves[0]
+        return
+
+    if len(leaves) == 2:
+        left, right = leaves
+        yield Node().add(left).add(right)
+        return
+
+    partition = Partition(leaves)
+
+    # Merge groups for triples
+    for triple in triples:
+        partition.union(*triple.ingroup)
+
+    _build_merge_fans(partition, fans)
+
+    # Try all possible mergings of the partition that fit the requested arity
+    for subpartition in partition.merge(size=arity):
+        _build_merge_fans(subpartition, fans)
+
+        if len(subpartition) <= 1:
+            return
+
+        # Recursively build subtrees for each group
+        for descendants in product(
+            *(
+                build(
+                    group,
+                    [triple for triple in triples if triple.is_in(group)],
+                    [fan for fan in fans if fan.is_in(group)],
+                    arity=arity,
+                )
+                for group in subpartition.values()
+            )
+        ):
+            yield Node().extend(descendants)
 
 
-def supertree(*trees: Node) -> Node | None:
+def supertree(*trees: Node, arity: int = 0) -> Generator[Node, None, None]:
     """
     Build a supertree from a set of phylogenetic trees.
 
-    The returned tree is the smallest tree compatible with every tree of
+    The first returned tree is the smallest tree compatible with every tree of
     the input, if such a tree exists.
 
     :param tree: any number of tree to build a supertree from
-    :returns: constructed tree, or None if input trees are incompatible
+    :param arity: arity of the generated supertrees, or 0 to generate
+        supertrees of arbitrary arity
+    :yields: possible supertrees, if any
     """
     # Use dictionaries as sets to merge parts while preserving ordering
     all_leaves = {}
@@ -161,7 +175,9 @@ def supertree(*trees: Node) -> Node | None:
         all_triples.update(dict.fromkeys(triples))
         all_fans.update(dict.fromkeys(fans))
 
-    return build(all_leaves.keys(), all_triples.keys(), all_fans.keys())
+    yield from build(
+        all_leaves.keys(), all_triples.keys(), all_fans.keys(), arity=arity
+    )
 
 
 def display(root: Node, leaves: Set[Node]) -> Node | None:
--- tests/sowing/comb/test_supertree.py
+++ tests/sowing/comb/test_supertree.py
@@ -93,122 +93,207 @@ def test_breakup():
 
 
 def test_build():
-    assert build([Node("a")], [], []) == Node("a")
+    assert list(build([Node("a")], [], [])) == [Node("a")]
 
-    assert build([Node("a"), Node("b")], [], []) == (
-        Node().add(Node("a")).add(Node("b"))
-    )
+    assert list(build([Node("a"), Node("b")], [], [])) == [
+        (Node().add(Node("a")).add(Node("b")))
+    ]
 
-    assert build([Node("a"), Node("b"), Node("c")], [], []) == (
-        Node().add(Node("a")).add(Node("b")).add(Node("c"))
-    )
+    assert list(build([Node("a"), Node("b"), Node("c")], [], [])) == [
+        Node().add(Node("a")).add(Node("b")).add(Node("c")),
+        Node().add(Node("a")).add(Node().add(Node("b")).add(Node("c"))),
+        Node().add(Node().add(Node("a")).add(Node("b"))).add(Node("c")),
+        Node().add(Node().add(Node("a")).add(Node("c"))).add(Node("b")),
+    ]
 
-    assert build(
-        [Node("a"), Node("b"), Node("c")],
-        [
-            Triple((Node("b"), Node("c")), Node("a")),
-        ],
-        [],
-    ) == (Node().add(Node("a")).add(Node().add(Node("b")).add(Node("c"))))
+    assert list(build([Node("a"), Node("b"), Node("c")], [], [], arity=2)) == [
+        Node().add(Node("a")).add(Node().add(Node("b")).add(Node("c"))),
+        Node().add(Node().add(Node("a")).add(Node("b"))).add(Node("c")),
+        Node().add(Node().add(Node("a")).add(Node("c"))).add(Node("b")),
+    ]
 
-    assert (
+    assert list(
         build(
             [Node("a"), Node("b"), Node("c")],
             [
                 Triple((Node("b"), Node("c")), Node("a")),
-                Triple((Node("b"), Node("a")), Node("c")),
             ],
             [],
         )
-        is None
-    )
-
-    assert build(
-        [Node("a"), Node("b"), Node("c"), Node("d")],
-        [
-            Triple((Node("a"), Node("b")), Node("c")),
-            Triple((Node("c"), Node("d")), Node("a")),
-        ],
-        [],
-    ) == (
-        Node()
-        .add(Node().add(Node("a")).add(Node("b")))
-        .add(Node().add(Node("c")).add(Node("d")))
-    )
+    ) == [Node().add(Node("a")).add(Node().add(Node("b")).add(Node("c")))]
 
     assert (
+        list(
+            build(
+                [Node("a"), Node("b"), Node("c")],
+                [
+                    Triple((Node("b"), Node("c")), Node("a")),
+                    Triple((Node("b"), Node("a")), Node("c")),
+                ],
+                [],
+            )
+        )
+        == []
+    )
+
+    assert list(
         build(
             [Node("a"), Node("b"), Node("c"), Node("d")],
             [
                 Triple((Node("a"), Node("b")), Node("c")),
                 Triple((Node("c"), Node("d")), Node("a")),
             ],
-            [Fan((Node("a"), Node("b"), Node("c")))],
+            [],
         )
-        is None
-    )
-
-    assert build(
-        [Node("b"), Node("c"), Node("d"), Node("e"), Node("f"), Node("z")],
-        [
-            Triple((Node("b"), Node("c")), Node("e")),
-            Triple((Node("e"), Node("f")), Node("z")),
-        ],
-        [
-            Fan((Node("b"), Node("c"), Node("d"))),
-            Fan((Node("b"), Node("e"), Node("z"))),
-        ],
-    ) == (
-        Node()
-        .add(Node().add(Node("b")).add(Node("c")).add(Node("d")))
-        .add(Node().add(Node("e")).add(Node("f")))
-        .add(Node("z"))
-    )
-
-
-def test_supertree():
-    assert supertree(
-        Node()
-        .add(Node().add(Node("a")).add(Node("b")))
-        .add(Node().add(Node("c")).add(Node("d"))),
-    ) == (
+    ) == [
         Node()
         .add(Node().add(Node("a")).add(Node("b")))
         .add(Node().add(Node("c")).add(Node("d")))
+    ]
+
+    assert (
+        list(
+            build(
+                [Node("a"), Node("b"), Node("c"), Node("d")],
+                [
+                    Triple((Node("a"), Node("b")), Node("c")),
+                    Triple((Node("c"), Node("d")), Node("a")),
+                ],
+                [Fan((Node("a"), Node("b"), Node("c")))],
+            )
+        )
+        == []
     )
 
-    assert supertree(
+    assert list(
+        build(
+            [Node("b"), Node("c"), Node("d"), Node("e"), Node("f"), Node("z")],
+            [
+                Triple((Node("b"), Node("c")), Node("e")),
+                Triple((Node("e"), Node("f")), Node("z")),
+            ],
+            [
+                Fan((Node("b"), Node("c"), Node("d"))),
+                Fan((Node("b"), Node("e"), Node("z"))),
+            ],
+        )
+    ) == [
         Node()
-        .add(Node().add(Node("a")), data="data left")
-        .add(Node().add(Node("b")), data="data right"),
-    ) == (Node().add(Node("a")).add(Node("b")))
+        .add(Node().add(Node("b")).add(Node("c")).add(Node("d")))
+        .add(Node().add(Node("e")).add(Node("f")))
+        .add(Node("z")),
+    ]
 
-    assert supertree(
-        Node()
-        .add(Node().add(Node("a")).add(Node("b")))
-        .add(Node().add(Node("c")).add(Node("d"))),
-        Node()
-        .add(Node().add(Node("a")).add(Node("b")))
-        .add(Node().add(Node("c")).add(Node("e"))),
-    ) == (
-        Node()
-        .add(Node().add(Node("a")).add(Node("b")))
-        .add(Node().add(Node("c")).add(Node("d")).add(Node("e")))
-    )
 
-    assert supertree(
+def test_supertree():
+    assert list(
+        supertree(
+            Node()
+            .add(Node().add(Node("a")).add(Node("b")))
+            .add(Node().add(Node("c")).add(Node("d"))),
+        )
+    ) == [
         Node()
         .add(Node().add(Node("a")).add(Node("b")))
-        .add(Node().add(Node("c")).add(Node("d"))),
+        .add(Node().add(Node("c")).add(Node("d")))
+    ]
+
+    assert list(
+        supertree(
+            Node()
+            .add(Node().add(Node("a")), data="data left")
+            .add(Node().add(Node("b")), data="data right"),
+        )
+    ) == [Node().add(Node("a")).add(Node("b"))]
+
+    assert list(
+        supertree(
+            Node()
+            .add(Node().add(Node("a")).add(Node("b")))
+            .add(Node().add(Node("c")).add(Node("d"))),
+            Node()
+            .add(Node().add(Node("a")).add(Node("b")))
+            .add(Node().add(Node("c")).add(Node("e"))),
+        )
+    ) == [
         Node()
         .add(Node().add(Node("a")).add(Node("b")))
-        .add(Node().add(Node("c")).add(Node("e"))),
-        Node().add(Node().add(Node("c")).add(Node("e"))).add(Node("d")),
-    ) == (
+        .add(Node().add(Node("c")).add(Node("d")).add(Node("e"))),
+        Node()
+        .add(Node().add(Node("a")).add(Node("b")))
+        .add(Node().add(Node("c")).add(Node().add(Node("d")).add(Node("e")))),
+        Node()
+        .add(Node().add(Node("a")).add(Node("b")))
+        .add(Node().add(Node().add(Node("c")).add(Node("d"))).add(Node("e"))),
+        Node()
+        .add(Node().add(Node("a")).add(Node("b")))
+        .add(Node().add(Node().add(Node("c")).add(Node("e"))).add(Node("d"))),
+    ]
+
+    assert list(
+        supertree(
+            Node()
+            .add(Node().add(Node("a")).add(Node("b")))
+            .add(Node().add(Node("c")).add(Node("d"))),
+            Node()
+            .add(Node().add(Node("a")).add(Node("b")))
+            .add(Node().add(Node("c")).add(Node("e"))),
+            Node().add(Node().add(Node("c")).add(Node("e"))).add(Node("d")),
+        )
+    ) == [
         Node()
         .add(Node().add(Node("a")).add(Node("b")))
         .add(Node().add(Node().add(Node("c")).add(Node("e"))).add(Node("d")))
-    )
+    ]
+
+    assert list(
+        supertree(
+            Node().add(Node("a")).add(Node("b")),
+            Node().add(Node("a")).add(Node().add(Node("c")).add(Node("d"))),
+        )
+    ) == [
+        Node().add(Node("a")).add(Node("b")).add(Node().add(Node("c")).add(Node("d"))),
+        Node().add(Node("a")).add(Node().add(Node("b")).add(Node("c")).add(Node("d"))),
+        Node()
+        .add(Node("a"))
+        .add(Node().add(Node("b")).add(Node().add(Node("c")).add(Node("d")))),
+        Node()
+        .add(Node("a"))
+        .add(Node().add(Node().add(Node("b")).add(Node("c"))).add(Node("d"))),
+        Node()
+        .add(Node("a"))
+        .add(Node().add(Node().add(Node("b")).add(Node("d"))).add(Node("c"))),
+        Node()
+        .add(Node().add(Node("a")).add(Node("b")))
+        .add(Node().add(Node("c")).add(Node("d"))),
+        Node()
+        .add(Node("b"))
+        .add(Node().add(Node("a")).add(Node().add(Node("c")).add(Node("d")))),
+    ]
+
+    assert list(
+        supertree(
+            Node().add(Node("a")).add(Node("b")),
+            Node().add(Node("a")).add(Node().add(Node("c")).add(Node("d"))),
+            arity=2,
+        )
+    ) == [
+        Node()
+        .add(Node("a"))
+        .add(Node().add(Node("b")).add(Node().add(Node("c")).add(Node("d")))),
+        Node()
+        .add(Node("a"))
+        .add(Node().add(Node().add(Node("b")).add(Node("c"))).add(Node("d"))),
+        Node()
+        .add(Node("a"))
+        .add(Node().add(Node().add(Node("b")).add(Node("d"))).add(Node("c"))),
+        Node()
+        .add(Node().add(Node("a")).add(Node("b")))
+        .add(Node().add(Node("c")).add(Node("d"))),
+        Node()
+        .add(Node("b"))
+        .add(Node().add(Node("a")).add(Node().add(Node("c")).add(Node("d")))),
+    ]
 
 
 def test_display():
